7c9ac99233b9da55309f8159f17389fbf5ab648e
Skwarek <oskarzonca24122008@gmail.com>
2024-12-26 22:52:08 +0100

    Make groups of enemies
    Better enemy destroing, Making groups of enemies without few enemies on each other bug

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<EnemySO> enemiesSOList = new List<EnemySO>();
+    private List<GameObject> EnemiesInGame = new List<GameObject>();
     private int whitchLine;
-    private EnemySO enemyToSpawn;
+    private EnemySO currentEnemySO;
     private int unitsForPixel = 100;
     private float halfScreenWidth = Screen.width / 2;
     private int numberOfLines = 3;
     private float lineHeight;
-    private GameObject enemyToDestroy;
 
     private void Awake()
     {
@@ -22,47 +23,73 @@ public class EnemySpawner : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            DeleteEnemy(enemyToDestroy);
-            CreateEnemy(enemyToSpawn);
+            DeleteAllEnemies();
+            CreateGroupOfEnemies(currentEnemySO);
         }
     }
 
-    private void CreateEnemy(EnemySO enemySO)
+    private Transform CreateEnemy(EnemySO enemySO)
     {
-        if (enemyToSpawn != null)
+        if (currentEnemySO != null)
         {
             Transform enemyPrefab = Instantiate(enemySO.prefab);
-            enemyToDestroy = enemyPrefab.gameObject;
             enemyPrefab.transform.position = GetEnemyStartPositionVector(enemySO.width);
-            SelectNewEnemy();
+            EnemiesInGame.Add(enemyPrefab.gameObject);
+            return enemyPrefab;
+        }
+        else
+        {
+            return null;
         }
     }
 
     private void SelectNewEnemy()
     {
         int whichEnemy = Random.Range(0, enemiesSOList.Count);
-        enemyToSpawn = enemiesSOList[whichEnemy];
-    }
+        currentEnemySO = enemiesSOList[whichEnemy];
 
-    private void DeleteEnemy(GameObject enemy)
-    {
-        if (enemy != null)
-        {
-            Destroy(enemy);
-        }
     }
 
     private Vector3 GetEnemyStartPositionVector(float enemyWidth)
     {
+        int line = Random.Range(-1, 1 + 1);
         float x = (halfScreenWidth / unitsForPixel) + (enemyWidth / unitsForPixel);
-        float y = SetEnemyYPosition(Random.Range(-1, 2));
-        Vector3 positionVector = new Vector3 (0, y, 0);
+        float y = lineHeight / unitsForPixel * line;
+        Vector3 positionVector = new Vector3(0, y, 0);
         return positionVector;
     }
 
-    private float SetEnemyYPosition(int line)
+    private float GetEnemyYPositionLine(GameObject enemy)
     {
-        float position = lineHeight / unitsForPixel * line;
-        return position;
+        return (enemy.transform.position.y * unitsForPixel) / lineHeight;
+    }
+
+    private void DeleteAllEnemies()
+    {
+        foreach (GameObject enemy in EnemiesInGame)
+        {
+            Destroy(enemy);
+        }
+        EnemiesInGame.Clear();
+    }
+
+    private void CreateGroupOfEnemies(EnemySO enemySO)
+    {
+        GameObject firstEnemy, secondEnemy;
+        while (true)
+        {
+            firstEnemy = CreateEnemy(enemySO).gameObject;
+            secondEnemy = CreateEnemy(enemySO).gameObject;
+            if (GetEnemyYPositionLine(firstEnemy) != GetEnemyYPositionLine(secondEnemy))
+            {
+                break;
+            }
+            else
+            {
+                Destroy(firstEnemy);
+                Destroy(secondEnemy);
+            }
+        }
+        SelectNewEnemy();
     }
 }
