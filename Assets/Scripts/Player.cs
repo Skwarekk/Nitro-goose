2004af1fc93ede20c4e71244964d207ac782a52a
Skwarek <skwarek.develop@gmail.com>
2025-01-10 19:26:32 +0100

    collision move bug fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,14 +21,45 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, inputVector.y, 0);
-
         Vector2 origin = new Vector3(transform.position.x, transform.position.y, 0);
         float correctSpeed = playerSpeed;
         if(gameInput.GetMovementVectorNormalized().x < 0)
         {
             correctSpeed = correctSpeed * playerBackwardDrag; 
         }
-        if (!Physics2D.BoxCast(origin, playerColliderSize, 0f, moveDir, playerSpeed * Time.deltaTime, wallsLayerMask))
+
+        bool canMove = !Physics2D.BoxCast(origin, playerColliderSize, 0f, moveDir, playerSpeed * Time.deltaTime, wallsLayerMask);
+
+        if (!canMove)
+        {
+            //cannot move towards moveDir
+            //attempt move only in X axis 
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics2D.BoxCast(origin, playerColliderSize, 0f, moveDirX, playerSpeed * Time.deltaTime, wallsLayerMask);
+            if (canMove)
+            {
+                //can move in X axis
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //cannot move in X axis
+                //attempt move only in Y axis
+                Vector3 moveDirY = new Vector3(0, moveDir.y, 0);
+                canMove = !Physics2D.BoxCast(origin, playerColliderSize, 0f, moveDirY, playerSpeed * Time.deltaTime, wallsLayerMask);
+                if (canMove)
+                {
+                    //can move in Y axis
+                    moveDir = moveDirY;
+                }
+                else
+                {
+                    //cannot move in any axis
+                }
+            }
+        }
+
+        if (canMove)
         {
             transform.position += moveDir * correctSpeed * Time.deltaTime;
         }
